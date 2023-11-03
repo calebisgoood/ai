242287644cc1e05245961236b36f06f36f6793e4
Ca <65434532+calebisgoood@users.noreply.github.com>
2023-11-04 07:22:49 +0800

    ai1online.py

--- ai1online.py
+++ ai1online.py
@@ -2,6 +2,8 @@ import speech_recognition as sr
 import os
 import pyttsx3
 import pyowm
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
 
 # Initialize the recognizer
 recognizer = sr.Recognizer()
@@ -11,6 +13,12 @@ engine = pyttsx3.init()
 
 # Initialize OpenWeatherMap API with your API key
 owm = pyowm.OWM("5c3e41ce46a8f8be1bb44517f216296d")
+
+# Initialize Spotify
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id="5db3f6d7b3c04429a4ad663987deba95",
+                                               client_secret="08cacb3605674f2bbccc2fc5da73e490",
+                                               redirect_uri="https://google.com",
+                                               scope="user-library-read user-read-playback-state user-modify-playback-state"))
 
 def speak(text):
     engine.say(text)
@@ -22,6 +30,15 @@ def get_weather(city):
     temperature = w.get_temperature('celsius')['temp']
     status = w.get_status()
     return f"The weather in {city} is {status} with a temperature of {temperature} degrees Celsius."
+
+def play_spotify_track(track_name):
+    results = sp.search(q=track_name, type="track")
+    if results["tracks"]["items"]:
+        track_uri = results["tracks"]["items"][0]["uri"]
+        sp.start_playback(uris=[track_uri])
+        speak(f"Playing {track_name} on Spotify.")
+    else:
+        speak(f"Sorry, I couldn't find the track {track_name} on Spotify.")
 
 while True:
     with sr.Microphone() as source:
@@ -38,6 +55,11 @@ while True:
                 city = recognizer.recognize_google(audio)
                 weather_info = get_weather(city)
                 speak(weather_info)
+            elif "play music" in command:
+                speak("What track would you like to play on Spotify?")
+                audio = recognizer.listen(source)
+                track_name = recognizer.recognize_google(audio)
+                play_spotify_track(track_name)
             elif "exit" in command:
                 speak("Goodbye!")
                 break
